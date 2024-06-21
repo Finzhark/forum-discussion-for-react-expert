5f7acf7dd3bf02d61a39c1f58aba9d7cd5b9ac75
C. Finza <126471175+Finzhark@users.noreply.github.com>
2024-06-21 12:56:22 +0700

    coba dikomentari saja

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,41 +16,40 @@ function Navbar() {
       dispatch(getProfileThunkAction());
     }
   }, [isLogin]);
-  // return (
-  //   <div className="navbar">
-  //     <Link to="/">Navbar</Link>
-  //     <div className="">
-  //       {/* Penambahan pengecekan null untuk profile */}
-  //       {profile && profile.avatar ? (
-  //         <div>
-  //           <img src={profile.avatar} alt="User Avatar" />
-  //           <p>{profile.name}</p>
-  //         </div>
-  //       ) : (
-  //         <p>Loading...</p>
-  //       )}
-  //       {isLogin ? (
-  //         <button onClick={handleLogout} type="button">Logout</button>
-  //       ) : (
-  //         <Link to="/login">Login</Link>
-  //       )}
-  //     </div>
-  //   </div>
-  // );
   return (
-    <div>
-      <div className="navbar">
-        <Link to="/">Forum Discuss</Link>
-        <div className="navbar-info">
-          <div className="navbar-user-info">
+    <div className="navbar">
+      <Link to="/">Navbar</Link>
+      <div className="">
+        {profile && profile.avatar ? (
+          <div>
             <img src={profile.avatar} alt="User Avatar" />
             <p>{profile.name}</p>
           </div>
-          {isLogin ? <button onClick={handleLogout} type="button">Logout</button> : <Link to="/login">Login</Link>}
-        </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+        {isLogin ? (
+          <button onClick={handleLogout} type="button">Logout</button>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
       </div>
     </div>
   );
+  // return (
+  //   <div>
+  //     <div className="navbar">
+  //       <Link to="/">Forum Discuss</Link>
+  //       <div className="navbar-info">
+  //         <div className="navbar-user-info">
+  //           <img src={profile.avatar} alt="User Avatar" />
+  //           <p>{profile.name}</p>
+  //         </div>
+  //         {isLogin ? <button onClick={handleLogout} type="button">Logout</button> : <Link to="/login">Login</Link>}
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default Navbar;
